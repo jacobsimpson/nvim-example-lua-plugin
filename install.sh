599ff4f900dc7b13d1c8cad9e06e49b0dfbdc35f
jacobsimpson <jacob.simpson@gmail.com>
2019-11-22 06:41:57 -0800

    Expand lua demo code (#6)
    * Expand on Lua demonstration code.
    
    * Change the instructions of copying/installing.

---
+++ install.sh
@@ -0,0 +1,35 @@
+#! /bin/bash
+
+dst=./nvim-example-lua-plugin
+if [[ ! -z $1 ]]; then
+    dst=$1
+    name=$(basename $1)
+fi
+
+mkdir -p "$dst"
+
+git clone --depth 1 \
+    https://github.com/jacobsimpson/nvim-example-lua-plugin \
+    "$dst"
+rm -Rf "$dst/.git" "$dst/install.sh" "$dst/README.asciidoc"
+mv "README-example.md" "README.md"
+
+if [[ ! -z $name ]]; then
+    mv "$dst/plugin/nvim-example-lua-plugin.vim" "$dst/plugin/$name.vim"
+    for f in $(find "$dst" -type f); do
+        printf ",s/nvim-example-lua-plugin/$dst/g\nw\n" | ed "$f" >& /dev/null
+    done
+fi
+
+echo "
+To try out your new plugin locally, use:
+
+nvim --cmd 'set rtp+=$dst'
+
+To use it after uploading to Github, add the necessary plugin load command to
+your Neovim config files.
+
+Example:
+
+Plug '<github-username>/${name}'
+"
